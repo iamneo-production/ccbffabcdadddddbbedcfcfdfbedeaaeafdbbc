367f97ed3bbf035bdcb825601417ff2a6d7b3f4d
Iamneo <techadmin@iamneo.ai>
2023-08-13 18:37:52 +0000

    Backup Commit

---
+++ reactapp/src/components/UI/Button/Button.js
@@ -0,0 +1,9 @@
+const Button = (props) => {
+    const { btnName = props.children,onClick,marked,style } = props;
+    return (
+    <button onClick={onClick} disabled={marked} className={style} >{ btnNa
+    me }</button>
+    )
+    }
+    export default Button;
+    
